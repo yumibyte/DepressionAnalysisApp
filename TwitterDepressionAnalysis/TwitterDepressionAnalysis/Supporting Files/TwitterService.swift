ca7e46e7bed29f72e0faf2e47501653a9cfed97c
Yumibyte <ashraigosa@gmail.com>
2020-07-12 01:16:25 -0700

    safari viewcontroller

--- TwitterDepressionAnalysis/TwitterDepressionAnalysis/Supporting Files/TwitterService.swift
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/Supporting Files/TwitterService.swift
@@ -10,18 +10,12 @@ import SwiftUI
 import Combine
 
 struct TwitterService {
-    
-    func valueForAPIKey(named keyname:String) -> String {
-      // Credit to the original source for this technique at
-      // http://blog.lazerwalker.com/blog/2014/05/14/handling-private-api-keys-in-open-source-ios-apps
-        let filePath = Bundle.main.path(forResource: "Keys", ofType: "plist")
-        let plist = NSDictionary(contentsOfFile:filePath!)
-        let value = plist?.object(forKey: keyname) as! String
-        return value
-    }
-    
-    lazy var TWITTER_CONSUMER_KEY = valueForAPIKey(named: "TWITTER_CONSUMER_KEY")
-    lazy var TWITTER_CONSUMER_SECRET = valueForAPIKey(named: "TWITTER_CONSUMER_SECRET")
+
+    let TWITTER_CONSUMER_KEY = FindAPIKey().valueForAPIKey(named: "TWITTER_CONSUMER_KEY")
+    let TWITTER_CONSUMER_SECRET = FindAPIKey().valueForAPIKey(named: "TWITTER_CONSUMER_SECRET")
     let TWITTER_URL_SCHEME = "twittersdk"
 
+}
+extension Notification.Name {
+    static let twitterCallback = Notification.Name(rawValue: "Twitter.CallbackNotification.Name")
 }
