7e53f855e46448006e2b49f9d6048d22918de374
Yumibyte <ashraigosa@gmail.com>
2020-07-12 00:26:28 -0700

    created XCode project

---
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterDepressionAnalysis
+//
+//  Created by Ashley Raigosa on 7/12/20.
+//  Copyright © 2020 Ashley Raigosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
