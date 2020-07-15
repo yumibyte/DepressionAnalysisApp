5cd442c1a21260927e946854159a14125b2f8bf6
Yumibyte <ashraigosa@gmail.com>
2020-07-14 22:53:09 -0700

    full database integrated. Created pretrained model

--- DepressionNLPModel/DepressionNLPModel.py
+++ DepressionNLPModel/DepressionNLPModel.py
@@ -5,39 +5,50 @@ Created on Tue Jul 14 19:09:04 2020
 
 @author: ashleyraigosa
 """
+    
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Depression_DB_Modified.csv')
+dataset = pd.read_csv('Depression_Reddit_Database_Filtered.csv')
 
 # Take key values for future processing
 X_key_vars = dataset.iloc[:, 3:8].values
 y = dataset.iloc[:, 8].values
 
 # Take solely text to be processed
-df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-X_title = df[:, 4]
+X_title = dataset.iloc[:, 4].values
+X_content = dataset.iloc[:, 7].values
 
-
-
+# Process text function
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 
-df = pd.read_csv('Depression_DB_Modified.xlsx', encoding="latin-1")
-df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-X = df['message']
-y = df['label']
+# Train Model
 cv = CountVectorizer()
-X = cv.fit_transform(X) # Fit the Data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_content = cv.fit_transform(X_content) # Fit the Data
+X_train, X_test, y_train, y_test = train_test_split(X_content, y, test_size=0.2, random_state=42)
 #Naive Bayes Classifier
 clf = MultinomialNB()
 clf.fit(X_train,y_train)
 clf.score(X_test,y_test)
 y_pred = clf.predict(X_test)
-print(classification_report(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+
+import joblib
+joblib.dump(clf, 'Depression_NLP_model.pkl') 
+
+
+
+#def ProcessText():
+    
+    #data = [X_input]
+    #vect = cv.transform(data).toarray()
+    #new_prediction = clf.predict(vect)
+
+    #print(new_prediction)
+
+
