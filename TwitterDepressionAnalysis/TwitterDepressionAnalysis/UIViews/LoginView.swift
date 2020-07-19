15443f503cce7bed35caddee8c6e19822a33a94a
Yumibyte <ashraigosa@gmail.com>
2020-07-19 10:36:04 -0700

    Login View with title and sign in with Twitter button

--- TwitterDepressionAnalysis/TwitterDepressionAnalysis/UIViews/LoginView.swift
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/UIViews/LoginView.swift
@@ -12,32 +12,43 @@ struct LoginView: View {
     @EnvironmentObject var twitter: TwitterService
     
     var body: some View {
-        ZStack(alignment: .center) {
-            Rectangle()
-                .frame(width: 340, height: 60)
-                .foregroundColor(Color(red: 80 / 255, green: 171 / 255, blue: 241 / 255))
-                .cornerRadius(10)
-            HStack {
-                Button(action: { self.twitter.authorize() }) {
-                    Text("Sign in with Twitter")
-                }
-                    Text(twitter.credential?.userId ?? "")
+        VStack {
+            Text("Login to Twitter Depression Analysis")
+                .minimumScaleFactor(0.5)
+
+                .font(.system(size: 30, weight: .medium, design: .default))
+                .multilineTextAlignment(.center)
+                .padding()
+                .offset(y: -90)
+            // Create twitter login button
+                ZStack(alignment: .center) {
+                    Rectangle()
+                        .frame(width: 340, height: 60)
+                        .foregroundColor(Color(red: 80 / 255, green: 171 / 255, blue: 241 / 255))
+                        .cornerRadius(10)
+                    HStack {
+                        Button(action: { self.twitter.authorize() }) {
+                            Text("Sign in with Twitter")
+                        }
+                        Text(twitter.credential?.userId ?? "")
                         Text(twitter.credential?.screenName ?? "")
                     }
                     .sheet(isPresented: self.$twitter.showSheet) {
                         CustomSafariViewController.SafariView(url: self.$twitter.authUrl)
-
+                        
                     }
                     .minimumScaleFactor(0.5)
                     .foregroundColor(.white)
                     .font(.system(size: 20, weight: .medium, design: .default))
                     .lineLimit(1)
                     .offset(x: 30)
-
-                Image("TwitterLogo")
-                    .scaleEffect(0.26)
-                .offset(x: -120)
+                    
+                    Image("TwitterLogo")
+                        .scaleEffect(0.26)
+                        .offset(x: -120)
+            }.offset(y: 80)
         }
+        
     }
 
 }
