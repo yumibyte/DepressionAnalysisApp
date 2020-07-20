ab4e9e98ae8e9d93354519d30f0d69960eee28b4
Yumibyte <ashraigosa@gmail.com>
2020-07-20 16:38:11 -0700

    navigation after logging twitter user in

---
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/UIViews/AnalyzeUserView.swift
@@ -0,0 +1,23 @@
+//
+//  AnalyzeUserView.swift
+//  TwitterDepressionAnalysis
+//
+//  Created by Ashley Raigosa on 7/20/20.
+//  Copyright © 2020 Ashley Raigosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnalyzeUserView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hi")
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+struct AnalyzeUserView_Previews: PreviewProvider {
+    static var previews: some View {
+            AnalyzeUserView()
+    }
+}
