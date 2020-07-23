27bd225f48d29c003e907c5d5f241180db1c6ca9
Yumibyte <ashraigosa@gmail.com>
2020-07-23 00:10:29 -0700

    code cleanup

--- TwitterDepressionAnalysis/TwitterDepressionAnalysis/UIViews/AnalyzeUserView.swift
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/UIViews/AnalyzeUserView.swift
@@ -14,34 +14,13 @@ struct AnalyzeUserView: View {
     
     let findAPIKey = FindAPIKey()
     @EnvironmentObject var twitter: TwitterService
-//    @State var profilePicture: String
     func createUser() {
-//        print(twitter.credential!.accessToken)
         TWTRTwitter.sharedInstance().logIn { (session, error) in
             let client = TWTRAPIClient.withCurrentUser()
             client.loadUser(withID: self.twitter.credential?.userId ?? "") { (user, error) in
                 let profilePicture = user?.profileImageURL
-                print(profilePicture)
             }
         }
-        
-//        let user = TWTRSession.init(authToken: twitter.credential?.accessToken ?? "", authTokenSecret: twitter.credential?.accessTokenSecret ?? "", userName: twitter.credential?.screenName ?? "", userID: twitter.credential?.userId ?? "")
-        
-//        let client = TWTRAPIClient.withCurrentUser()
-//        client.loadUser(withID: twitter.credential?.userId ?? "", completion: { (user, error) in
-//                           let profilePicture = user?.profileImageURL
-//        //                let profilePicture = NSLog(user!.profileImageURL)
-//                        print(profilePicture)
-//        })
-        
-//        let user = TWTRTwitter.sharedInstance().start(withConsumerKey: twitter.credential!.accessToken, consumerSecret: twitter.credential!.accessTokenSecret)
-//        let client = TWTRAPIClient.withCurrentUser()
-//            client.loadUser(withID: twitter.credential?.userId ?? "", completion: { (user, error) in
-//                   let profilePicture = user?.profileImageURL
-////                let profilePicture = NSLog(user!.profileImageURL)
-//                print(profilePicture)
-//            })
-//        print("hi")
     }
     
     
