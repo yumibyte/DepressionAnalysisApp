ade3c25ad572c11e9ca64ba394eaf4013ede9eda
Yumibyte <ashraigosa@gmail.com>
2020-07-30 21:20:13 -0700

    created class for tokenization/filtering

---
+++ TwitterDepressionAnalysis/TwitterDepressionAnalysis/Models/CleanTweet.swift
@@ -0,0 +1,9 @@
+//
+//  CleanTweet.swift
+//  TwitterDepressionAnalysis
+//
+//  Created by Ashley Raigosa on 7/30/20.
+//  Copyright © 2020 Ashley Raigosa. All rights reserved.
+//
+
+import Foundation
